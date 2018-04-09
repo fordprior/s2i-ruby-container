0184263be26f97cf186df020db57f9ffb79e6250
Ford Prior <ford.prior@snagajob.com>
2018-04-09 12:23:42 -0400

    Update app.rb

--- 2.4/test/puma-test-app/app.rb
+++ 2.4/test/puma-test-app/app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
 get '/' do
-  'Hello world!'
+  'hello world'
+end
+
+get '/*/format' do
+  # matches "GET /hello/reversed" and "GET /goodbye"
+  
+  case params['format']
+    when 'upcase'
+      "#{params[0].upcase}"
+    when 'reversed'
+      "#{params[0].reverse}"
+    else
+      "#{params[0]}"
+  end
+  
 end
