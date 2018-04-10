dae5a0250081ce8b50a5345e07d0dc3ac72ad989
Ford Prior <ford.prior@snagajob.com>
2018-04-10 13:41:21 -0400

    Update app_spec.rb

--- 2.4/test/puma-test-app/spec/app_spec.rb
+++ 2.4/test/puma-test-app/spec/app_spec.rb
@@ -1,8 +1,19 @@
-require File.expand_path '../spec_helper.rb', __FILE__
+ENV['RACK_ENV'] = 'test'
 
-describe "my sinatra app" do
-  it "should allow accessing the home page" do
+require '../../app.rb'
+require 'rspec'
+require 'rack/test'
+
+describe 'my sample ruby app' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
     get '/'
     expect(last_response).to be_ok
+    expect(last_response.body).to eq('hello world')
   end
 end
