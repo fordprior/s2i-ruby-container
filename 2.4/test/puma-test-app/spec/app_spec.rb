7ecaf1bae3732e39e72cdcc45a340e71e260b1b4
Ford Prior <ford.prior@snagajob.com>
2018-04-10 13:43:46 -0400

    Update app_spec.rb

--- 2.4/test/puma-test-app/spec/app_spec.rb
+++ 2.4/test/puma-test-app/spec/app_spec.rb
@@ -1,16 +1,6 @@
-ENV['RACK_ENV'] = 'test'
-
-require '../../app.rb'
-require 'rspec'
-require 'rack/test'
+require '../spec_helper.rb'
 
 describe 'my sample ruby app' do
-  include Rack::Test::Methods
-
-  def app
-    Sinatra::Application
-  end
-
   it "says hello" do
     get '/'
     expect(last_response).to be_ok
